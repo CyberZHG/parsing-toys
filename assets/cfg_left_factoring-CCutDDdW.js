061d4dad8efe8695b44b390dd4242c50989d3d0b
CyberZHG <CyberZHG@users.noreply.github.com>
2026-01-02 00:12:01 +0000

    deploy: 6a4c1498f0170742e9b4e0fbf1da06163213a76a

---
+++ assets/cfg_left_factoring-CCutDDdW.js
@@ -0,0 +1,13 @@
+import"./__vite-browser-external-CpYqa-Ba.js";import{s as o}from"./choose_examples-BjAtfSqC.js";import{C as r,g as S,u as l,s as i,a as m,b as c,c as p,d as s,e as E}from"./location_hash-_UlbSsyM.js";const u={example1:"Example 1: S -> i E t S | i E t S e S | a  E -> b",example2:"Example 2: S -> S S + | S S * | a",example3:"Example 3: S -> S + S | S S | ( S ) | S * | a",example4:"Example 4: A -> id | B | a  B -> C  C -> D  D -> id b"},d={example1:`S -> i E t S 
+   | i E t S e S 
+   | a
+E -> b`,example2:`S -> S S + 
+   | S S * 
+   | a`,example3:`S -> S + S 
+   | S S 
+   | ( S ) 
+   | S * 
+   | a`,example4:`A -> id | B | a
+B -> C
+C -> D
+D -> id b`},a=document.querySelector("#button-cfg-left-factoring");a.addEventListener("click",()=>{const t=document.querySelector("#cfg-left-factoring-error-message"),e=new r,n=S();e.parse(n)?(e.leftFactoring(),l(e.toString(),e.terminals(),e.nonTerminals()),t.parentElement.hidden=!0):(t.textContent=e.errorMessage(),t.parentElement.hidden=!1),i(n)});function x(t){s(d[t]),a.click()}o(u,x);m();c();p();s(E());a.click();
